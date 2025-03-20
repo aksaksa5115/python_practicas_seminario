f698025577e732216768af1788a44f6bd353fb5c
Martin Fuentes <martintati13@gmail.com>
2025-03-20 19:07:51 -0300

    correciones y añadido la compresion de las listas

--- proyecto_prueba_question/questions.py
+++ proyecto_prueba_question/questions.py
@@ -22,25 +22,29 @@ answers = [
 ]
 # Índice de la respuesta correcta para cada pregunta, el el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
+# se comprimen las listas para facilitar su entrada
+questions_to_ask = random.sample(list(
+    zip(questions, answers, correct_answers_index)), k=3)
 # El usuario deberá contestar 3 preguntas
 puntaje = 0
-for _ in range(3):
-    # Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions) - 1)
+for question, answer, correct_answers in questions_to_ask:
     # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
+    print(question)
+    # extraigo indice para acomodar la impresion de las respuestas
+    index = questions.index(question)
+    # imprimo las respuestas de forma limpia y ordenada
+    for i, answer in enumerate(answers[index]):
         print(f"{i + 1}. {answer}")
         # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
         respuesta = input('ingrese un numero entre 1 y 4 inclusive: ')
-        
+        # se le ruega al usuario que imprima una opcion valida
         while not respuesta.isdigit() or not (1 <= int(respuesta) <= 4):
             respuesta = input('ingrese un numero entre 1 y 4 valido: ')
         
         respuesta = int(respuesta) - 1
         # Se verifica si la respuesta es correcta
-        if respuesta == correct_answers_index[question_index]:
+        if respuesta == correct_answers:
             print("¡Correcto!")
             puntaje += 1
             break
@@ -48,9 +52,11 @@ for _ in range(3):
             # Si el usuario no responde correctamente después de 2 intentos,
             # se muestra la respuesta correcta
             puntaje += -0.5
-            print("Incorrecto. La respuesta correcta es:")
-            print(answers[question_index][correct_answers_index[question_index]])
+            print("incorrecto, intentalo de nuevo burro")
+            if intento == 1:
+                print("Incorrecto. La respuesta correcta es:")
+                print(correct_answers + 1)
         # Se imprime un blanco al final de la pregunta
-        print()
+    print()
 print('--------------------------------')
 print(f'juego terminado, tu puntaje fue de {puntaje}')
