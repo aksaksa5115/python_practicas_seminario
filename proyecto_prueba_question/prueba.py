3a5ec11265c8bc395dbc148434244f6455e58757
Martin <martintati13@gmail.com>
2025-03-20 18:47:29 -0300

    Delete proyecto_prueba_question/prueba.py

--- proyecto_prueba_question/prueba.py
+++
@@ -1,7 +0,0 @@
-respuesta = input("ingrese un numero del 1 al 4")
-
-while not respuesta.isdigit() or not (1 <= int(respuesta) <= 4):
-    respuesta = input("ingresa un numero valido del 1 al 4")
-
-respuesta = int(respuesta)
-print(f"elegiste la respuesta {respuesta}")
